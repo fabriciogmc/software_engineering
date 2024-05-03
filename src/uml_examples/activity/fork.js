2d95c8a590dadbf47d6de4cc8a5072003ab2f5b4
Fabricio Galende M. de Carvalho <fabriciogmc@gmail.com>
2024-05-03 17:38:58 -0300

    Synchronization function call exemplified in fork.js

--- src/uml_examples/activity/fork.js
+++ src/uml_examples/activity/fork.js
@@ -34,7 +34,19 @@ async function f2(id){
 }  
 
 
+/* The followint asynchronous function calls correspond
+** to a forked tasks with no join.
+*/
+
 f2(1);
 f2(2);
 f1(3);
 f1(4);
+
+/* The following call to method all synchronizes all 4 tasks.
+** because of it, the printed id is done in order, differently
+** from the previous case.
+*/
+Promise.all([task2(5), task1(6), task2(7), task1(8)]).then((values) => {
+  console.log(values);
+});
